097b2a1a8f18b55f1abc2536c79b7239d6908fd3
Trent Rand <contact@trentrand.com>
2022-01-27 21:46:37 -0700

    Add temporary script for generating static storage var identifiers
    Starknet requires storage variable accessors to use a computed _hash_
    rather than a standard ascii string of the variable name.
    
    The starknet.js library doesn't support generation of storage variable
    identifiers yet, so it is necessary to compute and copy these
    into source code manually when introducing new storage variables.
    
    In the future it probably makes sense to implement this into the library.

--- getStorageVarAddress.py
+++ getStorageVarAddress.py
@@ -1,4 +1,4 @@
 from starkware.starknet.public.abi import get_storage_var_address
 
-key = get_storage_var_address('canvasPixels')
+key = get_storage_var_address('canvas_pixels')
 print(f'key: {key}')
