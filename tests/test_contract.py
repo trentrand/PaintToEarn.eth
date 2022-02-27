2671efcb96809fc74489ac8067fe83cc4e595a24
Trent Rand <contact@trentrand.com>
2022-02-26 23:30:00 -0700

    Refactor test to use canvas contract fixture

--- tests/test_contract.py
+++ tests/test_contract.py
@@ -1,29 +1,51 @@
 import os
 
 import pytest
+from starkware.starknet.compiler.compile import compile_starknet_files
 from starkware.starknet.testing.starknet import Starknet
+from starkware.starknet.testing.contract import StarknetContract
+from starkware.starknet.testing.objects import StarknetTransactionExecutionInfo
+from starkware.starknet.testing.state import StarknetState
 
-CONTRACT_FILE = os.path.join("contracts", "contract.cairo")
+# Contract source
+CONTRACT_FILE = os.path.join(os.path.dirname(__file__), "../contracts/contract.cairo")
+
+@pytest.fixture
+async def canvasContract() -> StarknetContract:
+    contract_definition = compile_starknet_files([CONTRACT_FILE], debug_info=True)
+    state = await StarknetState.empty()
+    contract_address, execution_info = await state.deploy(
+        constructor_calldata=[],
+        contract_definition=contract_definition,
+    )
+    deploy_execution_info = StarknetTransactionExecutionInfo.from_internal(
+        tx_execution_info=execution_info, result=(), main_call_events=[]
+    )
+    assert contract_definition.abi is not None
+
+    return StarknetContract(
+        state=state,
+        abi=contract_definition.abi,
+        contract_address=contract_address,
+        deploy_execution_info=deploy_execution_info,
+    )
 
 @pytest.mark.asyncio
-async def test_update_canvas():
+async def test_update_canvas(canvasContract: StarknetContract):
  """Test update_canvas method."""
  # Create a new Starknet class that simulates the StarkNet system
  starknet = await Starknet.empty()
 
- # Deploy the contract.
- contract = await starknet.deploy(
-   source=CONTRACT_FILE,
- )
+ # Deploy the canvas contract.
 
  # Set initial canvas pixels
- await contract.update_canvas(
+ await canvasContract.update_canvas(
    indexes=[0,1,2,3,4,24],
    values=[1,2,3,4,5,6],
-   updates=6
+   updates=6,
  ).invoke()
 
- execution_info = await contract.get_array().call()
+ execution_info = await canvasContract.get_array().call()
  assert execution_info.result.arr[0] == 1
  assert execution_info.result.arr[1] == 2
  assert execution_info.result.arr[2] == 3
@@ -32,13 +54,13 @@ async def test_update_canvas():
  assert execution_info.result.arr[24] == 6
 
  # Overwrite existing canvas pixel values
- await contract.update_canvas(
+ await canvasContract.update_canvas(
    indexes=[0,5,23],
    values=[7,7,7],
    updates=3
  ).invoke()
 
- execution_info = await contract.get_array().call()
+ execution_info = await canvasContract.get_array().call()
  assert execution_info.result.arr[0] == 7
  assert execution_info.result.arr[1] == 2
  assert execution_info.result.arr[2] == 3
@@ -48,5 +70,5 @@ async def test_update_canvas():
  assert execution_info.result.arr[23] == 7
 
  # Check the result state
- execution_info = await contract.get_array().call()
+ execution_info = await canvasContract.get_array().call()
  print(execution_info.result.arr)
